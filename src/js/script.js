14a4c8a516b9c4eeffba717448afe52c13ebfb3d
DocOrNotADoc <leesunsinn@protonmail.com>
2021-01-24 22:54:00 +0300

    First. Basic layout. Smooth scroll to top & to links added (on clear JS)

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', () => {
+  
+  // Click logo to scroll to top
+
+  const mainLogo = document.getElementById('logo');
+  console.log(mainLogo);
+  mainLogo.addEventListener('click', => window.scrollTo({top: 0, behavior: 'smooth'}));
+
+  //Smooth Scrolling Using Navigation Menu
+
+  document.querySelectorAll('a[href^="#"').forEach(link => {
+
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        let href = this.getAttribute('href').substring(1);
+        const scrollTarget = document.getElementById(href);
+        const topOffset = document.querySelector('.header').offsetHeight;
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+  });
+
+});
