346ff5b4259c31eab83d69f829865330df5125cf
DocOrNotADoc <leesunsinn@protonmail.com>
2021-01-26 23:49:52 +0300

    shifting header in scroll down. Some style reworked. DONT REPEAT YOURSELF, need to re-factoring some shift-scripts!

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,13 @@
 window.addEventListener('DOMContentLoaded', () => {
   
+  // multi-used variables
+  const topOffset = document.querySelector('.header').offsetHeight;
+  const mainLogo = document.getElementById('logo');
+
+
   // Click logo to scroll to top
 
-  const mainLogo = document.getElementById('logo');
-  console.log(mainLogo);
+  // const mainLogo = document.getElementById('logo');
   mainLogo.addEventListener('click', () => { window.scrollTo({top: 0, behavior: 'smooth'}) });
 
   //Smooth Scrolling Using Navigation Menu
@@ -14,7 +18,7 @@ window.addEventListener('DOMContentLoaded', () => {
         e.preventDefault();
         let href = this.getAttribute('href').substring(1);
         const scrollTarget = document.getElementById(href);
-        const topOffset = document.querySelector('.header').offsetHeight;
+        // const topOffset = document.querySelector('.header').offsetHeight;
         const elementPosition = scrollTarget.getBoundingClientRect().top;
         const offsetPosition = elementPosition - topOffset;
         window.scrollBy({
@@ -23,5 +27,36 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     });
   });
+
+  // header shifting
+  const header = document.querySelector('.header'),
+    brand = document.querySelector('.header'),
+    wordMark = document.getElementById('word-mark');
+
+  window.addEventListener("scroll", callbackFunc);
+    function callbackFunc() {
+      let y = window.pageYOffset;
+      // function shifting(el){
+      //   if (y > 150) {
+      //     el.classList.toggle("navShadow");
+      //   } else {
+      //     el.classList.toggle("navShadow");
+      //   }
+      // }
+      // shifting(shifting);
+      // shifting(mainLogo);
+      // shifting(wordMark);
+      if (y > 150) {
+        header.classList.add("navShadow");
+        brand.classList.add("navShadow");
+        mainLogo.classList.add("navShadow");
+        wordMark.classList.add("navShadow");
+      } else {
+        header.classList.remove("navShadow");
+        brand.classList.remove("navShadow");
+        mainLogo.classList.remove("navShadow");
+        wordMark.classList.remove("navShadow");
+      }
+    }
 
 });
