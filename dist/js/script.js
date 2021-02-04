a4bf2902095b7a2c9043691fb501b06680794642
DocOrNotADoc <leesunsinn@protonmail.com>
2021-02-04 14:13:36 +0300

    tabs&mobile menu show&hide to tap hamburger. need to rework some styles

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,24 +1,22 @@
 window.addEventListener('DOMContentLoaded', () => {
   
   // multi-used variables
-  
   const mainLogo = document.getElementById('logo');
 
 
   // Click logo to scroll to top
-
   mainLogo.addEventListener('click', () => { window.scrollTo({top: 0, behavior: 'smooth'}) });
 
 
   //Smooth Scrolling Using Navigation Menu
-
   document.querySelectorAll('a[href^="#"').forEach(link => {
 
     link.addEventListener('click', function(e) {
         e.preventDefault();
         let href = this.getAttribute('href').substring(1);
         const scrollTarget = document.getElementById(href);
-        const topOffset = document.querySelector('.header').offsetHeight;
+        // const topOffset = document.querySelector('.header').offsetHeight;
+        const topOffset = 100;
         const elementPosition = scrollTarget.getBoundingClientRect().top;
         const offsetPosition = elementPosition - topOffset;
         window.scrollBy({
@@ -27,6 +25,9 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     });
   });
+
+  // при первом переходе берётся offsetPosition не от текущей высоты header, но от высоты header на момент клика.
+  // поэтому, на данный момент, значение offsetPosition захардкожено на значении высоты сжатого header-а (100px).
 
   // header shifting
   const header = document.querySelector('.header'),
@@ -44,13 +45,29 @@ window.addEventListener('DOMContentLoaded', () => {
       let y = window.pageYOffset;
 
       for (let i = 0; i < shiftChanges.length; i++) {
-
         
-        if (y > 150) {
+        if (y > 120) {
           shiftChanges[i].classList.add("navShadow");
         } else {
           shiftChanges[i].classList.remove("navShadow");
         }
       }
     }
+
+
+    //Toggle Menu on cellphones and tabs
+
+  const toggleMenuButton = document.getElementById('menu-toggle'),
+    togglableLinksList = document.getElementById('menu-header');
+    // togglableLinksList.addEventListener('click', () => { window.scrollTo({top: 0, behavior: 'smooth'}) });
+  toggleMenuButton.addEventListener('click', () => {
+
+    // if (toggleMenuButton.contains('closeMenu')) 
+
+    toggleMenuButton.classList.toggle('closeMenu');
+    togglableLinksList.classList.toggle('showMenu');
+  });
+
+  
+
 });
